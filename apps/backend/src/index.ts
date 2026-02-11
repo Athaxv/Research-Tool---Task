46286ea7d26a64e14ec2401616c57324a6b92c00
AtharvGaur <atharava112@gmail.com>
2026-02-11 21:25:47 +0530

    minor changes

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -13,6 +13,6 @@ app.get("/", async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000");
+app.listen(5000, () => {
+    console.log("Server started on port 5000");
 });
