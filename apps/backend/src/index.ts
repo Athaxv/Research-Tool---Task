7aacbeb30b2dc53759f6fe910c605c08e46e3889
AtharvGaur <atharava112@gmail.com>
2026-02-11 14:57:50 +0530

    psuh

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { WebSocketServer } from "ws";
 
 const app = express();
 
@@ -6,6 +7,22 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
     console.log("Server started on port 3000");
+});
+
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (ws) => {
+    ws.on("error", console.error);
+
+    ws.on("message", (data, isBinary) => {
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN && client !== ws) {
+                client.send(data, { binary: isBinary });
+            }
+        });
+    });
+
+    ws.send("Hello! Message From Server!!");
 });
